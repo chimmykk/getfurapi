a725644cc8e364af61d51f2ec22cfb91a88e2b7a
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2023-08-30 19:08:51 +0530

    test

---
+++ pullfur.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const ipfsFolder = './ipfs';
+
+app.get('/getfur/:filename', (req, res) => {
+    const filename = req.params.filename;
+    
+    if (!filename) {
+        return res.status(400).json({ error: "Missing filename parameter" });
+    }
+
+    try {
+        const jsonPath = path.join(ipfsFolder, filename);
+
+        if (!fs.existsSync(jsonPath)) {
+            return res.status(404).json({ error: "JSON file not found" });
+        }
+
+        const jsonContent = fs.readFileSync(jsonPath, 'utf8'); // Read the JSON file as text
+        const jsonData = JSON.parse(jsonContent); // Parse the JSON content
+
+        const attributes = jsonData.attributes || [];
+        const furAttribute = attributes.find(attr => attr.trait_type === "Fur");
+
+        if (!furAttribute) {
+            return res.status(404).json({ error: "Fur attribute not found" });
+        }
+
+        const furValue = furAttribute.value;
+        res.json({furValue });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
